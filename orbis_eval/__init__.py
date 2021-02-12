b98bb56fd96b7d0a531e0ed4998086396a847f07
roger <climberwoodi@gmx.ch>
2021-02-12 07:19:20 +0100

    chg: update to version 2.3.0

--- orbis_eval/__init__.py
+++ orbis_eval/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __name__ = "orbis_eval"
-__version__ = "2.2.4"
+__version__ = "2.3.0"
 __author__ = "fabod"
 __year__ = "2019"
 __description__ = "An Extendable Evaluation Pipeline for Named Entity Drill-Down Analysis"
--- orbis_eval/__version__.py
+++ orbis_eval/__version__.py
@@ -1,5 +1,5 @@
 __name__ = "orbis_eval"
-__version__ = "2.2.4"
+__version__ = "2.3.0"
 __author__ = "fabod"
 __year__ = "2019"
 __description__ = "An Extendable Evaluation Pipeline for Named Entity Drill-Down Analysis"
